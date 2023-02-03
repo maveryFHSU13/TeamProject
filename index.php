3b1b4a3c3cb0182ffa4ac7830bd68fa7b109d4f7
maveryFHSU13 <124326500+maveryFHSU13@users.noreply.github.com>
2023-02-03 10:58:19 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Meal Planner</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+    <header>
+        <h1>Problem Diagnosis</h1>
+    </header>
+    <p>Getting a balanced and nutritious diet on the table for three meals a day every day 
+        can be difficult when you don’t know where to start. The task of making sure 
+        you and your family have a balanced diet has many obstacles that need to be overcome
+</p>
+    <h1>Proposed Treatment</h1>
+    <p>Our daily and weekly meal planner app/website will involve the following:
+</p>
+
+<h1> Plan of Work </h1>
+<p> 1.1 Frontend
+Pages could include the following:
+Login/signup – allow the user to access or create an account; this may involve email/password, or connect to another service like Google. 
+Dashboard – will be displayed as the default page after login, and contains information such as the next recipe (if set ahead of time), specifications for factors such as goal daily calories, allergies, daily meal times (allowing for other setups/diets that do not use the usual 3 meals), and often viewed and/or recommended recipes.
+</p>
+<p>
+Task distribution
+Cheron Fletcher – frontend / organization
+Jasmine Albert – frontend / JavaScript
+Gilberto Guerra, Michael Avery – backend
+Sheun Badmus, Gary Hoehler, Hope Beasley – ?
+
+</p>
+
+</body>
+
+</html>
